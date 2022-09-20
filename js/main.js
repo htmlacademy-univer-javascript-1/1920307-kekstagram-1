04e6ae75bc4a0f510d0f54bfde53001c66c5b0cc
Anastasia Demina <fioB7@yandex.ru>
2022-09-20 20:14:59 +0500

    Добавлены методы для случайного целого числа и проверки длины строки

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function getRandomIntNumber(min, max) {
+  if (min > max) {
+    const intermediary = min;
+    min = max;
+    max = intermediary;
+  }
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
+
+function checkLength(string, maxLenght) {
+  return string.length <= maxLenght;
+}
