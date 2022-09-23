fdc4dce6fcea6637d934f6fb21502d3e27cf03f4
Keks <keks@htmlacademy.ru>
2022-09-23 19:11:55 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,15 @@
+function getRandomIntNumber(min, max) {
+  if (min > max) {
+    const intermediary = min;
+    min = max;
+    max = intermediary;
+  }
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
+
+function checkLength(string, maxLenght) {
+  return string.length <= maxLenght;
+}
+
+getRandomIntNumber(10, 50);
+checkLength('abcd', 5);
